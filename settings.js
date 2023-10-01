77cfca2de230dfd632cb3222921bf5a9adfb21ef
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-10-01 14:59:58 +0200

    fixed url

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://cars-dat3week2.azurewebsites.net/api"
+export const API_URL = "https://agreeable-mushroom-05935b103.3.azurestaticapps.net/api"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
